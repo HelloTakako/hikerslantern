cc0f17bda9529afb595895f1176309e4f7522920
Takako <24834291+HelloTakako@users.noreply.github.com>
2019-01-23 00:12:44 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// "Animsition"
+// I(Takako) don't write the whole code below, I just copied and pasted codes from a tutorial website and modified only the values to adjust.
+
+$(document).ready(function() {
+  $(".animsition").animsition({
+    inClass               :   'fade-in-up-sm',
+    inDuration            :    1000,
+    linkElement           :   '.animsition-link',
+    loading               :    true,
+    loadingParentElement  :   'div#wrapper',
+    loadingClass          :   'animsition-loading',
+    unSupportCss          : [ 'animation-duration',
+                              '-webkit-animation-duration',
+                              '-o-animation-duration'
+                            ],
+    overlay               :   false,
+    overlayClass          :   'animsition-overlay-slide',
+    overlayParentElement  :   'div#wrapper'
+  });
+});
+
+
+// w3schools Slideshow : https://www.w3schools.com/howto/howto_js_slideshow.asp
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
